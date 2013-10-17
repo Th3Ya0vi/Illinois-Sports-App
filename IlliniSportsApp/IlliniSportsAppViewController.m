693a487824b495d3105b4ce39c2280b83855c58a
Osayd Abdu <Osayd@wirelessprvnat-172-16-253-185.near.illinois.edu>
2013-10-17 15:42:48 -0500

    adding more HelloWorld

--- IlliniSportsApp/IlliniSportsAppViewController.m
+++ IlliniSportsApp/IlliniSportsAppViewController.m
@@ -30,6 +30,8 @@
     // Initialize homeTeams, need to retrieve data from Online
     homeTeam = [NSArray arrayWithObjects:@"Illini", @"Michigan", @"Illini", @"Illini", @"Rochester", @"NorthWestern", @"Michigan", @"Illini", nil];
     Hello World
+     Hello World 
+     Life is tough 
     
     // Initialize awayTeam, also need to get data online
     awayTeam = [NSArray arrayWithObjects:@"Rochester", @"Illini", @"Michigan", @"NorthWestern", @"Illini", @"Illini", @"Illini", @"NorthWestern", nil];
