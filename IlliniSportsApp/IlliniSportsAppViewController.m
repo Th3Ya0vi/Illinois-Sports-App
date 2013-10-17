9068e5017434e58841caeb53301cba5649adc6db
Matthew Liu <Matt@wirelessprvnat-172-16-174-10.near.illinois.edu>
2013-10-17 15:35:11 -0500

    duplicated hello world

--- IlliniSportsApp/IlliniSportsAppViewController.m
+++ IlliniSportsApp/IlliniSportsAppViewController.m
@@ -30,6 +30,7 @@
     // Initialize homeTeams, need to retrieve data from Online
     homeTeam = [NSArray arrayWithObjects:@"Illini", @"Michigan", @"Illini", @"Illini", @"Rochester", @"NorthWestern", @"Michigan", @"Illini", nil];
      Hello World
+     Hello World
     
     // Initialize awayTeam, also need to get data online
     awayTeam = [NSArray arrayWithObjects:@"Rochester", @"Illini", @"Michigan", @"NorthWestern", @"Illini", @"Illini", @"Illini", @"NorthWestern", nil];
